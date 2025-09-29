a24897065fbbe0151785bfd36c8e0a623a356bd6
drylmrls <darylcrismorales@proton.me>
2025-09-29 21:01:59 +0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,9 +3,9 @@
 pkg install python
 pkg install git
 pkg install nmap
-pkg install wget
 git clone https://github.com/shahrilnet/remote_lua_loader.git
 cd remote_lua_loader/payloads
 wget https://github.com/etaHEN/etaHEN/releases/download/2.3B/etaHEN-2.3B.bin
 cd ../..
+wget https://github.com/drylmrls/termux_ps5_loader/raw/refs/heads/main/script.sh
 chmod -R 700 *
