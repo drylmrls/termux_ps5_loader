40013c1820dca5d8c7b4c372ade6dec1d0932cfb
drylmrls <darylcrismorales@proton.me>
2025-09-29 21:27:54 +0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,7 @@
 pkg install python
 pkg install git
 pkg install nmap
+pkg install wget
 git clone https://github.com/shahrilnet/remote_lua_loader.git
 cd remote_lua_loader/payloads
 wget https://github.com/etaHEN/etaHEN/releases/download/2.3B/etaHEN-2.3B.bin
