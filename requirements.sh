7b0148011201a7f5d7c2e3edaf9d9df2618d4da7
drylmrls <darylcrismorales@proton.me>
2025-09-29 20:52:58 +0800

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+# This installs all the required applications and files
+
+pkg install python
+pkg install git
+pkg install nmap
+pkg install wget
+git clone https://github.com/shahrilnet/remote_lua_loader.git
+cd remote_lua_loader/payloads
+wget https://github.com/etaHEN/etaHEN/releases/download/2.3B/etaHEN-2.3B.bin
+cd ../..
+chmod -R 700 *
